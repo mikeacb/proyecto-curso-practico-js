3aff674b237607d2ab7869c8f2c74c141425e7fb
mikeacb <ex.bex@hotmail.com>
2022-11-09 16:42:16 -0600

    Archivos de la clase 19

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const emailMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const bgMenuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 emailMenu.addEventListener('click', emailMenuState);
+bgMenuIcon.addEventListener('click', bgMenuState);
 
 function emailMenuState() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function bgMenuState() {
+    mobileMenu.classList.toggle('inactive');
 }
