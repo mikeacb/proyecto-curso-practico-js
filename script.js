5eb32c4b45b6eec39abaf63ecf005fc4351a67b1
mikeacb <ex.bex@hotmail.com>
2022-11-09 17:21:04 -0600

    Archivos de la clase 20

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
+// Desktop Menu
 const emailMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const bgMenuIcon = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
 
 emailMenu.addEventListener('click', emailMenuState);
-bgMenuIcon.addEventListener('click', bgMenuState);
 
 function emailMenuState() {
     desktopMenu.classList.toggle('inactive');
+    aside.classList.toggle('inactive');
 }
 
+// Mobile Menu
+const bgMenuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+bgMenuIcon.addEventListener('click', bgMenuState);
+
 function bgMenuState() {
+    aside.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
+}
+
+// Cart Menu
+const cartMenuIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+cartMenuIcon.addEventListener('click', cartMenuStatus);
+
+function cartMenuStatus() {
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
 }
